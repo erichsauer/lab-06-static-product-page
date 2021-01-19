bd1796db4bfc7fdba3f8253bf57ae9adf7019957
ERICH SAUER <erich.sauer@gmail.com>
2021-01-19 14:06:01 -0800

    sets up initial file structure

---
+++ products/data.js
@@ -0,0 +1,11 @@
+export const animals = [
+    {
+        id: 1,
+        price: 3,
+        image: 'crested-fluffle.png',
+        category: 'fragrant',
+        size: 'large',
+        name: 'Crested Fluffle',
+        
+    },
+];
